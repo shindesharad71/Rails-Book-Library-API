87e92addbafa7ef8fb771fdee69393e1455012ec
Sharad Shinde <shindesharad71@gmail.com>
2021-09-07 19:40:53 +0530

    Added: Book Methods

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,7 +1,47 @@
 class Api::V1::BooksController < ApplicationController
+
+  # Index renders all items from books table
   def index
+    books = Book.all
+    render json: books, status: 200
   end
 
-  def show
+  # Create action will create new book record with given data
+  def create
+    book = Book.new(
+      name: prod_params[:name],
+      author: prod_params[:author],
+      description: prod_params[:description],
+      release_date: prod_params[:release_date]
+    )
+
+    if book.save
+      render json: book, status: 200
+    else
+      render json: {error: "Error creating new book record"}
+    end
   end
+
+  # Show method will look up book by id
+  def show
+    book = Book.find_by(id: params[:id])
+
+    if book
+      render json: book, status: 200
+    else
+      render json: {error: "Book not found"}
+    end
+  end
+
+
+  # This is private method only available to this controller
+  private
+    def prod_params
+      params.require(:book).permit([
+        :name,
+        :author,
+        :description,
+        :release_date
+      ])
+    end
 end
