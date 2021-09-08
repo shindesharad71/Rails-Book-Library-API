e20953af0b6de45566e13013ad8176724c9f339a
Sharad Shinde <shindesharad71@gmail.com>
2021-09-08 21:21:23 +0530

    Added: Create User

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::UsersController < ApplicationController
     def create
         @user = User.create(user_params)
         if @user.valid?
-            token = encode_token(user_id, @user.id)
+            token = encode_token({user_id: @user.id})
             render json: {user: @user, token: token}, status: 201
         else
             render json: {error: "Invalid username or password"}
