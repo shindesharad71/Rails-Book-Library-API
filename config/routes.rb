9875fcf9c86748b3cb73ce53e477fc70b363af0e
Sharad Shinde <shindesharad71@gmail.com>
2021-09-08 20:07:58 +0530

    Updated: CORS Config

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :books, only: [:index, :show, :create]
+
+      resources :users, only: [:create]
+      post "/login", to: "users#login"
+      post "/auto-login", to: "users#auto_login"
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
