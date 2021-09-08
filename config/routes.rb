e0e2fac264aa482372717512b34b8908a59cc14b
Sharad Shinde <shindesharad71@gmail.com>
2021-09-08 21:08:10 +0530

    Fixed: Routing Issue

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
       resources :books, only: [:index, :show, :create]
       resources :users, only: [:create]
 
-      post "/login", to: "users#login"
-      get "/auto-login", to: "users#auto_login"
+      post "/users/login", to: "users#login"
+      get "/users/auto-login", to: "users#auto_login"
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
