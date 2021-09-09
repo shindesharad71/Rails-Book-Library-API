f09f73bacb164f998f661305a4c0a57899b45ac4
Sharad Shinde <shindesharad71@gmail.com>
2021-09-09 18:36:23 +0530

    Added: More Auth Related Methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,26 @@
 class ApplicationController < ActionController::API
 
+    before_action :authorized
+
+    JWT_SECRET = '8I@={Lfygi#!eFY'
+
     def encode_token(payload)
-        JWT.encode(payload, '8I@={Lfygi#!eFY') # Add your JWT Secret key here
+        JWT.encode(payload, JWT_SECRET) # Add your JWT Secret key here
+    end
+
+    def auth_header
+        request.headers['Authorization']
+    end
+
+    def decode_token
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                JWT.decode(token, JWT_SECRET, true, algorithm: 'HS256')
+            rescue JWT::DEcodeError
+                nil
+            end
+        end
     end
 
 end
